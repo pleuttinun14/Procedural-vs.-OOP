6599d81bd5963557b82c4f886bb015e053b7a153
Pat <pleuttinunp@gmail.com>
2025-08-12 18:11:54 +0700

    Update 2
    Add part 3 that doesn't finish

---
+++ Part_three.py
@@ -0,0 +1,27 @@
+class Onlineshop:
+  def __init__(self, name ,url):
+    self.name = str(name)
+    self.url = str(url)
+    self.productss = {}
+"""  
+  def addingItemsToCart(self, customer, product, quantity):
+    self.customer(Customer)
+    
+
+class Product:
+  def __init__(self, name_product, description, price):
+    self.name_product = str(name_product)
+    self.description = str(description)
+    self.price = float(price)
+    self.online_shop(Onlineshop)
+
+class Customer:
+  def __init__(self, name_customer, email, address):
+    self.name_customer = str(name_customer)
+    self.email = str(email)
+    self.address = str(address)
+    self.cart = {}
+    self.past_orders = {}
+"""
+Store = Onlineshop()
+Store = "Gaming"
